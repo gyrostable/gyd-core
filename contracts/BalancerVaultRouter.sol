7d40b6c32ef23fc2b89cc83245d51173a9a2fbf7
Lewis Gudgeon <ljfgudgeon@gmail.com>
2021-09-22 12:06:09 +0100

    Balancer vault router

---
+++ contracts/BalancerVaultRouter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "../libraries/DataTypes.sol";
+import "../interfaces/IVaultRouter.sol";
+import "../interfaces/IVault.sol";
+import "../interfaces/ILPTokenExchangerRegistry.sol";
+import "../interfaces/ILPTokenExchanger.sol";
+import "./BaseVaultRouter.sol";
+
+/// @title Subclass of BaseVaultRouter to manage Balancer Vaults
+contract BaseVaultRouter is BaseVaultRouter {
+
+}
