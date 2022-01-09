e0f99cbe8373c346ee0020ed57c71d2948d48a38
Daniel Perez <daniel@perez.sh>
2022-01-09 15:38:12 +0000

    Fix profiler

--- contracts/testing/profiling/LogExpMathProfiler.sol
+++ contracts/testing/profiling/LogExpMathProfiler.sol
@@ -4,15 +4,9 @@ pragma solidity ^0.8.10;
 import "../../../libraries/LogExpMath.sol";
 
 contract LogExpMathProfiler {
-    function profileSqrt(uint256[] calldata values)
-        external
-        pure
-        returns (uint256 sqrt)
-    {
+    function profileSqrt(uint256[] calldata values) external returns (uint256 sqrt) {
         for (uint256 i = 0; i < values.length; i++) {
-            LogExpMath.sqrt(values[i]);
+            sqrt = LogExpMath.sqrt(values[i]);
         }
-
-        return sqrt;
     }
 }
