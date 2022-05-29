adb52bb849d6c3b499706f96efddfad86de5f92a
Daniel Perez <daniel@perez.sh>
2022-05-30 00:22:23 +0100

    Use two-phase commit for governor change
    CU-2dac83q[closed]

--- interfaces/IGovernable.sol
+++ interfaces/IGovernable.sol
@@ -5,10 +5,19 @@ interface IGovernable {
     /// @notice Emmited when the governor is changed
     event GovernorChanged(address oldGovernor, address newGovernor);
 
+    /// @notice Emmited when the governor is change is requested
+    event GovernorChangeRequested(address newGovernor);
+
     /// @notice Returns the current governor
     function governor() external view returns (address);
+
+    /// @notice Returns the pending governor
+    function pendingGovernor() external view returns (address);
 
     /// @notice Changes the governor
     /// can only be called by the current governor
     function changeGovernor(address newGovernor) external;
+
+    /// @notice Called by the pending governor to approve the change
+    function acceptGovernance() external;
 }
