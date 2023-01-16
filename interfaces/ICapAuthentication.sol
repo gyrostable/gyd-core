3b06faa72f33bcd5d46c692b8a2be50fe753eb75
Daniel Perez <daniel@perez.sh>
2023-01-16 13:21:53 +0000

    Fix license

--- interfaces/ICapAuthentication.sol
+++ interfaces/ICapAuthentication.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: LicenseRef-Gyro-1.0
+// for information on licensing please see the README in the GitHub repository <https://github.com/gyrostable/core-protocol>.
 pragma solidity ^0.8.4;
 
 /// @notice ICapAuthentication handles cap authentication for the capped protocol
