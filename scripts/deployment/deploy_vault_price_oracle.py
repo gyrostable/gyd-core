80e67d642ddf400c628715bb74e479439e40d3c8
Daniel Perez <daniel@perez.sh>
2023-02-06 18:38:29 +0000

    Update deployment

--- scripts/deployment/deploy_vault_price_oracle.py
+++ scripts/deployment/deploy_vault_price_oracle.py
@@ -15,18 +15,15 @@ def cpmm():
 
 
 @with_gas_usage
-@as_singleton(Balancer2CLPPriceOracle)
 def g2clp():
     get_deployer().deploy(Balancer2CLPPriceOracle, **make_tx_params())
 
 
 @with_gas_usage
-@as_singleton(Balancer3CLPPriceOracle)
 def g3clp():
     get_deployer().deploy(Balancer3CLPPriceOracle, **make_tx_params())
 
 
 @with_gas_usage
-@as_singleton(BalancerECLPPriceOracle)
 def eclp():
     get_deployer().deploy(BalancerECLPPriceOracle, **make_tx_params())
