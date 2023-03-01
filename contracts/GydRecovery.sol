6f1bd6fe6d7f60b5ae7e878cbd57c63e4362dc6c
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2023-03-01 23:47:45 +0100

    Some fixes, polishing

--- contracts/GydRecovery.sol
+++ contracts/GydRecovery.sol
@@ -22,23 +22,24 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
     using EnumerableSet for EnumerableSet.UintSet;
 
     /* We account for burn actions (= when the mechanism is triggered) in two ways:
-    - For partial burns, where less than the whole underlying supply is burnt, we store the percentage that has been burned in a cumulative adjustment factor. We can convert from and to "adjusted" amounts by multiplying/dividing by the adjustment factor. The "staked amounts" for liquidity mining are in units of "adjusted amounts".
-    - For full burns, where the whole supply is burnt, no such 1:1 conversion is possible. Instead, we store a counter to distinguish whether this has happened, and we also store some history.
-    Adjusted amounts are only meaningful when the last full-burn-id is stored as well. All this is important to correctly perform accounting for withdrawals and liquidity mining.
+    - For partial burns, where less than the whole underlying supply is burnt, we store the percentage that has been burned in a cumulative adjustment factor. We can convert from and to "adjusted" amounts by multiplying/dividing by the adjustment factor. The "staked amounts" for liquidity mining are in units of *adjusted amounts*.
+    - For full burns, where the whole supply is burnt, the conversion is not 1:1. Instead, we store a counter to distinguish whether a full burn has happened since the last update, and we also store some history.
+    Adjusted amounts are only meaningful when the last full-burn-id is stored as well. All this is important to correctly perform accounting for withdrawals and for rewards.
     */
 
     /// @dev Max burn factor. If less than this share would be left over after burning, we do a full burn instead. This is to avoid numerical instability in the adjustment factor.
     uint256 internal constant MAX_PARTIAL_BURN_FACTOR = 0.01e18;
 
-    /** @dev Full (marked for withdrawal and not) amount for a given user. We store the adjusted amount *not* marked for
-     * withdrawal in _perUserStaked from LiquidityMining. We account for that fact that _perUserStaked can become outdated
-     * in userCheckpoint().
+    /** @dev Full (marked for withdrawal and not) amount for a given address. We store the adjusted amount *not* marked for
+     * withdrawal in _perUserStaked from LiquidityMining. We account for the fact that _perUserStaked can become outdated
+     * in userCheckpoint() and in the related view methods.
      */
     struct Position {
         uint256 lastUpdatedFullBurnId;
+        // TODO the following var is currently write-only. Remove unless we want to provide an (optional) view method to read this.
         uint256 adjustedAmount;
     }
-    // Owner -> Position
+    // owner -> Position
     mapping(address => Position) internal positions;
 
     struct FullBurnInfo {
@@ -57,7 +58,7 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         address to;
     }
     mapping (uint256 => PendingWithdrawal) internal pendingWithdrawals;
-    // user's address -> pending withdrawal ids. Convenience feature.
+    // address -> pending withdrawal ids. Mostly a convenience feature.
     mapping(address => EnumerableSet.UintSet) internal userPendingWithdrawalIds;
     uint256 internal nextWithdrawalId;
     uint256 public withdrawalWaitDuration;
@@ -69,10 +70,10 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
     IGYDToken public immutable gydToken;
     IERC20 public immutable rewardToken;
 
-    event Deposit(address beneficiary, uint256 amount);
-    event WithdrawalQueued(uint256 id, address to, uint256 withdrawalAt, uint256 amount);
-    event WithdrawalCompleted(address to, uint256 amount);
-    event RecoveryExecuted(uint256 tokensBurned, bool fullBurn);
+    event Deposit(address beneficiary, uint256 adjustedAmount, uint256 amount);
+    event WithdrawalQueued(uint256 id, address to, uint256 withdrawalAt, uint256 adjustedAmount, uint256 amount);
+    event WithdrawalCompleted(uint256 id, address to, uint256 adjustedAmount, uint256 amount);
+    event RecoveryExecuted(uint256 tokensBurned, bool isFullBurn, uint256 newAdjustmentFactor);
 
     constructor(
         address _governor,
@@ -91,7 +92,7 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         withdrawalWaitDuration = _duration;
     }
 
-    /// @dev Total amount available for burning. Includes amounts that are marked for withdrawal but not yet withdrawn.
+    /// @dev Total amount available for burning. Includes amounts marked for withdrawal but not yet withdrawn.
     function totalUnderlying() public view returns(uint256)
     {
         return gydToken.balanceOf(address(this));
@@ -109,6 +110,16 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         adjustedAmount = positions[account].lastUpdatedFullBurnId < nextFullBurnId ? 0 : _perUserStaked[account];
     }
 
+    function adjustedAmountToAmount(uint256 adjustedAmount) external view returns (uint256 amount)
+    {
+        return adjustedAmount.mulDown(adjustmentFactor);
+    }
+
+    function amountToAdjustedAmount(uint256 amount) external view returns (uint256 adjustedAmount)
+    {
+        return amount.divDown(adjustmentFactor);
+    }
+
     function deposit(uint256 amount) external
     {
         return depositFor(msg.sender, amount);
@@ -118,12 +129,11 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
     {
         gydToken.safeTransferFrom(msg.sender, address(this), amount);
 
-        uint256 newAdjustedAmount = amount.divDown(adjustmentFactor);
-        _stake(beneficiary, newAdjustedAmount);  // This also handles full burns, which is important below.
+        uint256 adjustedAmount = amount.divDown(adjustmentFactor);
+        _stake(beneficiary, adjustedAmount);  // This also handles full burns, which is important for the next line.
+        positions[beneficiary].adjustedAmount += adjustedAmount;
 
-        positions[beneficiary].adjustedAmount += newAdjustedAmount;
-
-        emit Deposit(beneficiary, amount);
+        emit Deposit(beneficiary, adjustedAmount, amount);
     }
 
     function initiateWithdrawal(uint256 amount) external returns (uint256 withdrawalId)
@@ -131,14 +141,14 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         return initiateWithdrawalAdjusted(amount.divDown(adjustmentFactor));
     }
 
-    /// @notice Like initiateWithdrawal() but operates on "adjusted" amounts. Convenient to withdraw all funds
-    /// via `initiateWithdrawalAdjusted(balanceAdjustedOf(account))` without worrying about rounding errors.
+    /// @notice Like initiateWithdrawal() but operates on adjusted amounts. Convenient to withdraw all funds
+    /// via `initiateWithdrawalAdjusted(balanceAdjustedOf(account))` without worrying about rounding issues.
     function initiateWithdrawalAdjusted(uint256 adjustedAmount) public returns (uint256 withdrawalId)
     {
-        _unstake(msg.sender, adjustedAmount);  // This also handles full burns, which is important below.
+        // redundant with _unstake() but we want a better error message.
+        require(adjustedAmount <= balanceOf(msg.sender), "not enough to withdraw");
 
-        require(adjustedAmount <= _perUserStaked[msg.sender], "not enough to withdraw");
-        _perUserStaked[msg.sender] -= adjustedAmount;
+        _unstake(msg.sender, adjustedAmount);  // This also handles full burns, which is important below.
 
         withdrawalId = nextWithdrawalId;
         ++nextWithdrawalId;
@@ -149,13 +159,14 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
             to: msg.sender
         });
         pendingWithdrawals[withdrawalId] = withdrawal;
-        userPendingWithdrawalIds[msg.sender].add(withdrawalId);
+        userPendingWithdrawalIds[withdrawal.to].add(withdrawalId);
 
         emit WithdrawalQueued(
             withdrawalId,
             withdrawal.to,
             withdrawal.withdrawableAt,
-            adjustedAmount  // TODO make events consistent: amount or adjusted. But adjusted looks ok. Emit event there.
+            adjustedAmount,
+            adjustedAmount.mulDown(adjustmentFactor)
         );
     }
 
@@ -171,7 +182,6 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         );
 
         if (pending.createdFullBurnId < nextFullBurnId) {
-            // It's all been burned.
             delete pendingWithdrawals[withdrawalId];
             userPendingWithdrawalIds[pending.to].remove(withdrawalId);
             return 0;
@@ -185,7 +195,7 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         delete pendingWithdrawals[withdrawalId];
         userPendingWithdrawalIds[pending.to].remove(withdrawalId);
 
-        emit WithdrawalCompleted(pending.to, amount);
+        emit WithdrawalCompleted(withdrawalId, pending.to, adjustedAmount, amount);
     }
 
     function listPendingWithdrawals(
@@ -199,6 +209,9 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         return pending;
     }
 
+    /// @dev Update rewards accounting for user (see LiquidityMining.userCheckpoint()) and also update their
+    /// position data in case of a full burn. It may look a bit ugly that these two things are mixed here but it's
+    /// important to update them at the same time.
     function userCheckpoint(address account) public override
     {
         globalCheckpoint();
@@ -206,24 +219,21 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         Position storage position = positions[account];
         uint256 lastUpdatedFullBurnId = position.lastUpdatedFullBurnId;
 
-        if (lastUpdatedFullBurnId == 0)
-            return;  // invalid / new
-
         uint256 perUserStaked = _perUserStaked[account];
         uint256 totalStakedIntegral;
-        if (lastUpdatedFullBurnId < nextFullBurnId) {
-            // Full burn happened since last update.
+        if (lastUpdatedFullBurnId > 0 && lastUpdatedFullBurnId < nextFullBurnId) {
+            // The user receives rewards for their staked (= not withdrawal-initiated) amount until the first burn after
+            // the last userCheckpoint() and we update their position going forward.
             totalStakedIntegral = fullBurnHistory[lastUpdatedFullBurnId].totalStakedIntegral;
 
-            // Update stored data
             position.adjustedAmount = 0;
-            _perUserStaked[account] = 0;
             position.lastUpdatedFullBurnId = nextFullBurnId;
+            delete _perUserStaked[account];
         } else {
+            // No full burn since last userCheckpoint(). The user receives rewards until now.
             totalStakedIntegral = _totalStakedIntegral;
         }
 
-        // stakedBalanceOf() would yield the same result as perUserStaked but consume additional gas.
         _perUserShare[account] += perUserStaked.mulDown(
             totalStakedIntegral - _perUserStakedIntegral[account]
         );
@@ -260,13 +270,13 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         rewardsEmissionEndTime = endTime;
     }
 
-    /// @dev To stop liquidity mining early and/or have the amount reimbursed when liquidity mining was paused due to insufficient balance for a while.
+    /// @dev To stop liquidity mining early and/or have the amount reimbursed when liquidity mining was paused when the pool was empty for a while.
     function stopLiquidityMining(address reimbursementTo) external governanceOnly
     {
         globalCheckpoint();
         uint256 reimbursementAmount = rewardToken.balanceOf(address(this)) - _totalUnclaimedRewards;
         rewardToken.safeTransfer(reimbursementTo, reimbursementAmount);
-        rewardsEmissionEndTime = block.timestamp - 1;
+        rewardsEmissionEndTime = 0;
     }
 
     function rewardsEmissionRate() public view override returns (uint256)
@@ -280,16 +290,19 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         return amount;
     }
 
+    /// @dev Whether or not the recovery module should run and would run next time it's called.
     function shouldRun(DataTypes.ReserveState memory reserveState) public view returns (bool)
     {
-        if (totalUnderlying() == 0)
+        if (totalUnderlying() == 0) {
+            // *Not* a corner case! This happens after a full burn!
             return false;
+        }
         uint256 currentCR = reserveState.totalUSDValue.divDown(gydToken.totalSupply());
         uint256 triggerCR = gyroConfig.getUint(ConfigKeys.GYD_RECOVERY_TRIGGER_CR);
         return currentCR < triggerCR;
     }
 
-    /** @dev check if the recovery module should run, do it if needed. Can be called by anyone.
+    /** @dev Check if the recovery module should run; do it if needed. Can be called by anyone.
      * @return has run? */
     function checkAndRun(DataTypes.ReserveState memory reserveState) public returns (bool)
     {
@@ -302,13 +315,12 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         uint256 currentGYDSupply = gydToken.totalSupply();
 
         if (targetGYDSupply >= currentGYDSupply) {
-            // Sanity check. This can only happen if TARGET_CR < TRIGGER_CR, which is probably a buggy config.
+            // Sanity check. This can only happen if GYD_RECOVERY_TARGET_CR < GYD_RECOVERY_TRIGGER_CR, which is probably a buggy config.
             return false;
         }
         uint256 amountToBurn = currentGYDSupply - targetGYDSupply;
 
         executeBurn(amountToBurn);
-
         return true;
     }
 
@@ -320,6 +332,7 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
         return checkAndRun(reserveState);
     }
 
+    /// @dev Burn `amountToBurn` or the whole pool, whichever is smaller. Do proper accounting.
     function executeBurn(uint256 amountToBurn) internal
     {
         globalCheckpoint();
@@ -335,17 +348,18 @@ contract GydRecovery is IGydRecovery, Governable, LiquidityMining {
             ++nextFullBurnId;
             adjustmentFactor = FixedPoint.ONE;
 
-            // The following makes totalStaked inconsistent with _perUserStaked but not with stakedBalanceActiveOf().
+            // The following makes totalStaked inconsistent with _perUserStaked but this is accounted for in userCheckpoint(), balanceAdjustedOf(), etc.
             totalStaked = 0;
         } else {
             adjustmentFactor = adjustmentFactor.mulDown(_totalUnderlying - amountToBurn).divDown(_totalUnderlying);
-            // NB In extreme cases, when many partial burns occurred, this may make adjustmentFactor = 0 due to numerical
+            // NB In extreme cases, when many large partial burns occurred, this may make adjustmentFactor = 0 due to numerical
             // error. In this case, no further deposits or withdrawals are possible. This situation is hard to avoid while
             // keeping the contract's accounting right. Governance should monitor adjustmentFactor to notice this condition
             // ahead of time, then ask contributors to withdraw funds, and deploy a new instance of the contract.
+            // TODO ^ To be discussed.
         }
 
         gydToken.burn(amountToBurn);
-        emit RecoveryExecuted(amountToBurn, isFullBurn);
+        emit RecoveryExecuted(amountToBurn, isFullBurn, adjustmentFactor);
     }
 }
