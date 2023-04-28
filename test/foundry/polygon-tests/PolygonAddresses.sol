ba3db20c72d32b9bb86d3f980f8413a283bcca37
Josh Guha <joshguha@gmail.com>
2023-04-28 12:38:54 +0100

    Finish mint tests

--- test/foundry/polygon-tests/PolygonAddresses.sol
+++ test/foundry/polygon-tests/PolygonAddresses.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.13;
 contract PolygonAddresses {
     address constant motherboardAddress = 0x68BDeE1bF95AD730F379A05eB8c51fb5dFA07748;
     address constant reserveManagerAddress = 0xa9bD43E98b184C66AFb83F75348eB041df81BB15;
+    address constant balancerVaultAddress = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
 }
