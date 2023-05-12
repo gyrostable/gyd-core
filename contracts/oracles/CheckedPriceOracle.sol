4ce891a73da734d6de9d95fd7c18ffa27bb30815
Daniel Perez <daniel@perez.sh>
2023-05-12 18:20:54 +0200

    Improve checked price oracle naming

--- contracts/oracles/CheckedPriceOracle.sol
+++ contracts/oracles/CheckedPriceOracle.sol
@@ -23,7 +23,7 @@ contract CheckedPriceOracle is IUSDPriceOracle, IUSDBatchPriceOracle, Governable
     using SafeCast for uint256;
     using SafeCast for int256;
 
-    uint256 public constant INITIAL_MAX_RELATIVE_WETH_DEVIATION = 0.02e18;
+    uint256 public constant INITIAL_MAX_PCT_WETH_USD_DEVIATION = 0.02e18;
     uint256 public constant INITIAL_RELATIVE_EPSILON = 0.02e18;
     uint256 public constant MAX_RELATIVE_EPSILON = 0.1e18;
 
@@ -32,7 +32,7 @@ contract CheckedPriceOracle is IUSDPriceOracle, IUSDBatchPriceOracle, Governable
     IUSDPriceOracle public usdOracle;
     IRelativePriceOracle public relativeOracle;
 
-    uint256 public maxRelativeWethDeviation;
+    uint256 public maxPctWethUsdDeviation;
 
     uint256 public relativeEpsilon;
 
@@ -76,7 +76,7 @@ contract CheckedPriceOracle is IUSDPriceOracle, IUSDBatchPriceOracle, Governable
         relativeOracle = IRelativePriceOracle(_relativeOracle);
         relativeEpsilon = INITIAL_RELATIVE_EPSILON;
         wethAddress = _wethAddress;
-        maxRelativeWethDeviation = INITIAL_MAX_RELATIVE_WETH_DEVIATION;
+        maxPctWethUsdDeviation = INITIAL_MAX_PCT_WETH_USD_DEVIATION;
     }
 
     function setUSDOracle(address _usdOracle) external governanceOnly {
@@ -282,11 +282,8 @@ contract CheckedPriceOracle is IUSDPriceOracle, IUSDBatchPriceOracle, Governable
         relativeEpsilon = _relativeEpsilon;
     }
 
-    function setRelativeAbsoluteWethDeviation(uint256 _relativeAbsoluteWethDeviation)
-        external
-        governanceOnly
-    {
-        maxRelativeWethDeviation = _relativeAbsoluteWethDeviation;
+    function setMaxPctWethUsdDeviation(uint256 _maxPctWethUsdDeviation) external governanceOnly {
+        maxPctWethUsdDeviation = _maxPctWethUsdDeviation;
     }
 
     function _checkPriceLevel(
@@ -296,10 +293,7 @@ contract CheckedPriceOracle is IUSDPriceOracle, IUSDBatchPriceOracle, Governable
     ) internal view {
         uint256 trueWETH = getRobustWETHPrice(signedPrices, priceLevelTwaps);
         uint256 relativePriceDifference = priceLevel.absSub(trueWETH).divDown(trueWETH);
-        require(
-            relativePriceDifference <= maxRelativeWethDeviation,
-            Errors.ROOT_PRICE_NOT_GROUNDED
-        );
+        require(relativePriceDifference <= maxPctWethUsdDeviation, Errors.ROOT_PRICE_NOT_GROUNDED);
     }
 
     function _ensureRelativePriceConsistency(
