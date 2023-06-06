e4fbc95d8ae34b936c669072a5016e13ad032336
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2023-06-06 21:04:18 +0200

    tests and fuzz tests for derived value invariants

--- test/foundry/Pamm.t.sol
+++ test/foundry/Pamm.t.sol
@@ -18,6 +18,9 @@ contract PammTest is Test {
     // irrelevant
     uint64 public constant OUTFLOW_MEMORY = 999993123563518195;
 
+    uint public constant DELTA_SMALL = 1;  // 1e-18
+    uint public constant DELTA_MED = 1e10; // 1e-8
+
     address public constant governorAddress = address(0);
     GyroConfig internal gyroConfig;
     TestingPAMMV1 internal tpamm;
@@ -33,7 +36,64 @@ contract PammTest is Test {
         // ^ Use tpamm.setParams() to set actual parameters!
     }
 
-    // TODO test some invariants about derived values, e.g. xl=1
+    function testDerivedValuesExamples() public {
+        checkDerivedValues(1e18, 0.3e18, 0.6e18);
+        checkDerivedValues(1e18, 0.3e18, 0.6e18);
+        checkDerivedValues(1e18, 0.3e18, 0.6e18);
+        checkDerivedValues(1e18, 0.3e18, 0.6e18);
+        checkDerivedValues(1e18, 0.3e18, 0.6e18);
+        checkDerivedValues(1e18, 0.3e18, 0.6e18);
+        checkDerivedValues(0.5e18, 0.3e18, 0.6e18);
+        checkDerivedValues(0.3e18, 0.3e18, 0.6e18);
+        checkDerivedValues(0.3e18, 0.3e18, 0.6e18);
+        checkDerivedValues(0.3e18, 0.3e18, 0.6e18);
+        checkDerivedValues(0.3e18, 0.3e18, 0.6e18);
+        checkDerivedValues(2.0e18, 0.3e18, 0.6e18);
+        checkDerivedValues(0.3e18, 0.5e18, 0.3e18); // II l does not exist and we're in II ii
+    }
+
+    /// @dev map x from [0, type max] to [a, b]
+    function mapToInterval(uint32 x, uint a, uint b) public pure returns (uint) {
+        // order matters b/c integers!
+        return a + (b - a) * uint(x) / type(uint32).max;
+    }
+
+    /// forge-config: default.fuzz.runs = 10000
+    function testFuzz_derivedValues(uint32 alphaBar0, uint32 xuBar0, uint32 thetaBar0) public {
+        // Transmogrify values into the range we need.
+        // NB anything larger than uint64.max ~ 18.4 (unscaled) is pointless b/c values are cast down to uint64 when stored in params.
+        uint alphaBar = mapToInterval(alphaBar0, 0.0001e18, type(uint64).max);
+        uint xuBar = mapToInterval(xuBar0, 0.0001e18, 1e18 - 0.0001e18);
+        uint thetaBar = mapToInterval(thetaBar0, 0.0001e18, 1e18 - 0.0001e18);
+
+        console.log("alphaBar = %e", alphaBar);
+        console.log("xuBar = %e", xuBar);
+        console.log("thetaBar = %e", thetaBar);
+
+        checkDerivedValues(alphaBar, xuBar, thetaBar);
+    }
+
+    function checkDerivedValues(uint alphaBar, uint xuBar, uint thetaBar) public {
+        IPAMM.Params memory params = IPAMM.Params(uint64(alphaBar), uint64(xuBar), uint64(thetaBar), OUTFLOW_MEMORY);
+        tpamm.setParams(params);
+        PrimaryAMMV1.DerivedParams memory derived = tpamm.computeDerivedParams();
+
+        // Some simple invariants. We can't do too much here b/c our only spec is the PAMM implementation itself.
+        if (derived.baThresholdRegionI > derived.baThresholdIIHL && derived.baThresholdIIHL > derived.baThresholdRegionII) {
+            // TODO when this test passes consistently, remove xlThresholdIIHL and replace by 1.
+            assertApproxEqAbsDecimal(derived.xlThresholdIIHL, 1e18, DELTA_MED, 18);
+            // xu is non-trivial!
+        } else {
+            assertEq(derived.xlThresholdIIHL, 0);
+        }
+
+        if (derived.baThresholdRegionII > derived.baThresholdIIIHL) {
+            assertApproxEqAbsDecimal(derived.xlThresholdIIIHL, 1e18, DELTA_MED, 18);
+            // TODO alpha should just be theta = 1 - theta_bar.
+        } else {
+            assertEq(derived.xlThresholdIIIHL, 0);
+        }
+    }
 
     function testRegionReconstructionExamples() public {
         // From previous (brownie) tests
