a9309263477f8d0a6dee8cdf990521d7c0edc91f
Daniel Perez <daniel@perez.sh>
2023-07-11 20:01:03 +0100

    Deploy gyro config on mainnet

--- scripts/deployment/deploy_proxy_admin.py
+++ scripts/deployment/deploy_proxy_admin.py
@@ -12,4 +12,4 @@ from scripts.utils import (
 @as_singleton(ProxyAdmin)
 def main():
     deployer = get_deployer()
-    deployer.deploy(ProxyAdmin, **make_tx_params())
+    deployer.deploy(ProxyAdmin, **make_tx_params(), publish_source=True)
