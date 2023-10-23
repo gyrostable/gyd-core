d26f82ec1be1e233b84871a275f5550a00697d1a
Daniel Perez <daniel@perez.sh>
2023-10-23 15:25:19 +0100

    Deploy reserve stewardship incentives

--- scripts/deployment/deploy_reserve_stewardship_incentives.py
+++ scripts/deployment/deploy_reserve_stewardship_incentives.py
@@ -4,15 +4,53 @@ from scripts.utils import (
     make_tx_params,
     with_deployed,
 )
-from brownie import ReserveStewardshipIncentives, GovernanceProxy  # type: ignore
+from brownie import ReserveStewardshipIncentives, GovernanceProxy, GyroConfig
+from tests.support import config_keys
+from tests.support import constants  # type: ignore
+
+from tests.support.constants import MAINNET_GOVERNANCE_ADDRESS
 
 
 @with_deployed(GovernanceProxy)
-def main(governance_proxy):
+@with_deployed(GyroConfig)
+def main(gyro_config, governance_proxy):
     deployer = get_deployer()
-    deployer.deploy(
+    reserve_stewardship_incentives = deployer.deploy(
         ReserveStewardshipIncentives,
-        governance_proxy,
+        MAINNET_GOVERNANCE_ADDRESS,
         get_gyro_config(),
         **make_tx_params(),
+    )
+
+    governance_proxy.executeCall(
+        gyro_config,
+        gyro_config.setAddress.encode_input(
+            config_keys.STEWARDSHIP_INC_ADDRESS, reserve_stewardship_incentives
+        ),
+        {"from": deployer, **make_tx_params()},
+    )
+
+    governance_proxy.executeCall(
+        gyro_config,
+        gyro_config.setUint.encode_input(
+            config_keys.STEWARDSHIP_INC_DURATION, constants.STEWARDSHIP_INC_DURATION
+        ),
+        {"from": deployer, **make_tx_params()},
+    )
+
+    governance_proxy.executeCall(
+        gyro_config,
+        gyro_config.setUint.encode_input(
+            config_keys.STEWARDSHIP_INC_MAX_VIOLATIONS,
+            constants.STEWARDSHIP_INC_MAX_VIOLATIONS,
+        ),
+        {"from": deployer, **make_tx_params()},
+    )
+
+    governance_proxy.executeCall(
+        gyro_config,
+        gyro_config.setUint.encode_input(
+            config_keys.STEWARDSHIP_INC_MIN_CR, constants.STEWARDSHIP_INC_MIN_CR
+        ),
+        {"from": deployer, **make_tx_params()},
     )
