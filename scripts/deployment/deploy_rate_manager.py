37debbd2480575abe7c30aca861809bd0df75244
Daniel Perez <daniel@perez.sh>
2023-10-23 17:29:32 +0100

    Set rate providers

--- scripts/deployment/deploy_rate_manager.py
+++ scripts/deployment/deploy_rate_manager.py
@@ -20,18 +20,20 @@ def main(governance_proxy):
 
 
 providers_info = [
-    RateProviderInfo(
-        underlying=TokenAddresses.sDAI,
-        provider="0xc7177B6E18c1Abd725F5b75792e5F7A3bA5DBC2c",
+    (
+        TokenAddresses.sDAI,
+        RateProviderInfo(
+            underlying=TokenAddresses.DAI,
+            provider="0xc7177B6E18c1Abd725F5b75792e5F7A3bA5DBC2c",
+        ),
     ),
-    # RateProviderInfo(
-    #     underlying=TokenAddresses.aUSDT,
-    #     provider="",
-    # ),
-    # RateProviderInfo(
-    #     underlying=TokenAddresses.fUSDC,
-    #     provider="",
-    # ),
+    (
+        TokenAddresses.fUSDC,
+        RateProviderInfo(
+            underlying=TokenAddresses.USDC,
+            provider="0x8ee79Eb3f37b0ea4544DF2a0b9E228b6fcd8c718",
+        ),
+    ),
 ]
 
 
@@ -39,8 +41,9 @@ providers_info = [
 @with_deployed(GovernanceProxy)
 def set_rate_providers(governance_proxy, rate_manager):
     deployer = get_deployer()
-    governance_proxy.executeCall(
-        rate_manager,
-        rate_manager.setRateProviderInfo.encode_input(providers_info),
-        {"from": deployer, **make_tx_params()},
-    )
+    for asset, info in providers_info:
+        governance_proxy.executeCall(
+            rate_manager,
+            rate_manager.setRateProviderInfo.encode_input(asset, info),
+            {"from": deployer, **make_tx_params()},
+        )
